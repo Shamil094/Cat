ea8921448616e95818b91f258f2944b5ee78b2eb
Shamil Celilzade <shamillcelilzade@gmail.com>
2022-12-16 22:04:36 +0400

    Add project files.

---
+++ Cat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat
+{
+    internal class Cat
+    {
+        public delegate void OperationCat();
+        private string mood;    //Main methodunda istifade etmek ucun ne etmeli???
+                                //public olsa kenardan deyishmek olur//get-set(property kimi) ile set`i baglasaq methodla da deyismek olmur
+        private string hungry;
+        private string energy;
+
+        public void Play()
+        {
+            mood = "good";
+            hungry = "yes";
+            energy = "low";
+        }
+        public void Feed()
+        {
+            mood = "good";
+            hungry = "no";
+            energy = "high";
+        }
+        public void Sleep()
+        {
+            mood = "tired";
+            hungry = "yes";
+            energy = "high";
+        }
+        public void Apply(OperationCat OC)
+        {
+            OC.Invoke();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cat ct = new Cat();
+            ct.Apply(ct.Sleep);
+
+
+
+            Console.Read();
+        }
+    }
+}
